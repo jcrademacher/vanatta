91e0dcad7d513b5e91103637782557b668ad2d31
jradema <jradema@mit.edu>
2023-01-10 12:45:39 -0500

    updates regarding purui platform

--- src/matlab/purui/display_data.m
+++ src/matlab/purui/display_data.m
@@ -1,21 +1,27 @@
-sig_0 = read_complex_binary('../../rx_outputs/River PAB2 Van Atta 12-20-2022/fixed_vanatta4x2_nostag_006B_006F_006A_006C_x_001A_004A_004B_004D_chest_txfmr_nicktb_siggen_18,5kfc_0,0deg_8bit_pre_16bit_dat_prbs_0,5kbps_usrp_2,5m_depth_005B_purui_tx_60Vrms_18m_18m_1m_foam_sep_hphydro_diff_0.00.dat');
-sig = real(sig_0(24:end))-imag(sig_0(24:end));
-fs = 2e5;
+size = [7 7000000];
+
+id = fopen('../../../rx_outputs/River PAB2 Van Atta 01-10-2023/test_rx_01102022.dat','r');
+sig = fread(id,size,'float32').';
+
+ch1 = sig(:,1);
+
+fs = 192e3;
 fc = 18.5e3;
 
+% plot channel 1
 %sig = rx_baseband;
-t = [0:1/fs:length(sig)/fs-1/fs];
+t = [0:1/fs:length(ch1)/fs-1/fs];
 %sig = awgn(cos(2*pi*fc*t),30);
 figure(1);
 hold on;
-plot(sig);
+plot(ch1);
 
 
-window_size = floor(length(sig)/100);
+window_size = floor(length(ch1)/100);
 window = chebwin(window_size);
 Nfft = 2^nextpow2(fs*10);
 
-[pxx,f] = pwelch(sig,window,[],Nfft,fs,'power');
+[pxx,f] = pwelch(ch1,window,[],Nfft,fs);
 % 
 % max_search = [round(Nfft/fs*(fc-1)):round(Nfft/fs*(fc+1))];
 % [maxval,mindex] = max(pxx(max_search)); % max in each row
